98c1ea4d4953667cbeacb369a35f2f67b79a28f5
sawirricardo <sawir.ricardo@gmail.com>
2022-06-02 00:27:16 +0700

    webhook module

--- src/Webhook.php
+++ src/Webhook.php
@@ -2,8 +2,6 @@
 
 namespace Sawirricardo\Whatsapp;
 
-use Illuminate\Support\Facades\Response;
-
 class Webhook
 {
     /** @var string */
@@ -13,7 +11,20 @@ class Webhook
     private $verifyToken;
 
     /** @var ?callable */
-    private $onMessaged;
+    private $whenTexted;
+    private $whenTextedWithContact;
+    private $whenTextedWithLocation;
+    private $whenTextedWithMedia;
+    private $whenTextedWithSticker;
+
+    private $whenListReplied;
+    private $whenButtonsReplied;
+    private $whenQuickButtonsReplied;
+
+    private $whenMessageSent;
+    private $whenMessageDelivered;
+    private $whenMessageFailed;
+    private $whenMessageDeleted;
 
     public function __construct($token, $verifyToken)
     {
@@ -26,25 +37,213 @@ class Webhook
         return new static($token, $verifyToken);
     }
 
-    /** @param callable $whenMessageReceived */
-    public function whenMessageReceived($whenMessageReceived)
+    /** @param callable $whenTexted */
+    public function whenTexted($whenTexted)
     {
-        $this->onMessaged = $whenMessageReceived;
+        $this->whenTexted = $whenTexted;
+
+        return $this;
+    }
+
+    /** @param callable whenTextedWithMedia */
+    public function whenTextedWithMedia($whenTextedWithMedia)
+    {
+        $this->whenTextedWithMedia = $whenTextedWithMedia;
+
+        return $this;
+    }
+
+    /** @param callable whenTextedWithSticker */
+    public function whenTextedWithSticker($whenTextedWithSticker)
+    {
+        $this->whenTextedWithSticker = $whenTextedWithSticker;
+
+        return $this;
+    }
+
+    /** @param callable whenTextedWithLocation */
+    public function whenTextedWithLocation($whenTextedWithLocation)
+    {
+        $this->whenTextedWithLocation = $whenTextedWithLocation;
+
+        return $this;
+    }
+
+    /** @param callable whenTextedWithContact */
+    public function whenTextedWithContact($whenTextedWithContact)
+    {
+        $this->whenTextedWithContact = $whenTextedWithContact;
+
+        return $this;
+    }
+
+    /** @param callable whenUnknownMessageReceived */
+    public function whenUnknownMessageReceived($whenUnknownMessageReceived)
+    {
+        $this->whenUnknownMessageReceived = $whenUnknownMessageReceived;
+
+        return $this;
+    }
+
+    public function whenMessageDelivered($whenMessageDelivered)
+    {
+        $this->whenMessageDelivered = $whenMessageDelivered;
+
+        return $this;
+    }
+
+    public function whenMessageSent($whenMessageSent)
+    {
+        $this->whenMessageSent = $whenMessageSent;
+
+        return $this;
+    }
+
+    public function whenMessageFailed($whenMessageFailed)
+    {
+        $this->whenMessageFailed = $whenMessageFailed;
+
+        return $this;
+    }
+
+    public function whenMessageDeleted($whenMessageDeleted)
+    {
+        $this->whenMessageDeleted = $whenMessageDeleted;
+
+        return $this;
+    }
+
+    public function whenQuickButtonsReplied($whenQuickButtonsReplied)
+    {
+        $this->whenQuickButtonsReplied = $whenQuickButtonsReplied;
+
+        return $this;
+    }
+
+    public function whenButtonsReplied($whenButtonsReplied)
+    {
+        $this->whenButtonsReplied = $whenButtonsReplied;
+
+        return $this;
+    }
+
+    public function whenListReplied($whenListReplied)
+    {
+        $this->whenListReplied = $whenListReplied;
+
+        return $this;
+    }
+
+    public function whenUserNumberChanged($whenUserNumberChanged)
+    {
+        $this->whenUserNumberChanged = $whenUserNumberChanged;
 
         return $this;
     }
 
     /** @param callable || array<string, mixed> $data */
-    public function listen($data)
+    public function process($data)
     {
         if (is_callable($data)) {
             $data = $data();
         }
 
+        $metadata = data_get($data, 'entry.0.changes.0.value.metadata');
+        $contacts = data_get($data, 'entry.0.changes.0.value.contacts.0');
+        if (! is_null($status = data_get($data, 'entry.0.statuses.0'))) {
+            if ($status['status'] === 'delivered') {
+                call_user_func($this->whenMessageDelivered, $status);
+
+                return;
+            }
+            if ($status['status'] === 'sent') {
+                call_user_func($this->whenMessageSent, $status);
+
+                return;
+            }
+        }
+
+        if (! is_null($status = data_get($data, 'entry.0.changes.0.value.statuses.0'))) {
+            if ($status['status'] === 'failed') {
+                call_user_func($this->whenMessageFailed, $status);
+
+                return;
+            }
+        }
+
+        $message = data_get($data, 'entry.0.changes.0.value.messages.0');
+
+
+        if (array_key_exists('location', $message)) {
+            call_user_func($this->whenTextedWithLocation, $message);
+
+            return;
+        }
+
+        if (array_key_exists('contacts', $message)) {
+            call_user_func($this->whenTextedWithContact, $message);
+
+            return;
+        }
+
+        if ($message['type'] === 'unsupported') {
+            call_user_func($this->whenMessageDeleted, $message);
+
+            return;
+        }
+
+        if ($message['type'] === 'text') {
+            call_user_func($this->whenTexted, $message);
+
+            return;
+        }
+
+        if ($message['type'] === 'image') {
+            call_user_func($this->whenTextedWithMedia, $message);
+
+            return;
+        }
+
+        if ($message['type'] === 'sticker') {
+            call_user_func($this->whenTextedWithSticker, $message);
+
+            return;
+        }
+
+        if ($message['type'] === 'unknown') {
+            call_user_func($this->whenUnknownMessageReceived, $message);
+
+            return;
+        }
+
+        if ($message['type'] === 'button') {
+            call_user_func($this->whenQuickButtonsReplied, $message);
+
+            return;
+        }
+
+        if ($message['type'] === 'interactive') {
+            if (data_get($message, 'interactive.type') === 'list_reply') {
+                call_user_func($this->whenListReplied, $message);
+
+                return;
+            }
+
+            call_user_func($this->whenButtonsReplied, $message);
+
+            return;
+        }
+
+        if ($message['type'] === 'system') {
+            call_user_func($this->whenUserNumberChanged, $message);
+
+            return;
+        }
+
         call_user_func($this->onMessaged, $data);
     }
 
-    public function validate($data)
+    public function shouldProcess($data)
     {
         if (array_key_exists('hub', $data)) {
             $data = $data['hub'];
@@ -52,10 +251,10 @@ class Webhook
 
         if (array_key_exists('mode', $data) && $data['mode'] === 'subscribe') {
             if ($this->verifyToken === $data['verify_token']) {
-                return Response::make($data['challenge'], 200);
+                return true;
             }
         }
 
-        return Response::make('', 403);
+        return false;
     }
 }
--- tests/WebhookTest.php
+++ tests/WebhookTest.php
@@ -3,5 +3,20 @@
 use Sawirricardo\Whatsapp\Webhook;
 
 it('can listen for event', function () {
-    $webhook = Webhook::make('token', 'verifyToken');
+    Webhook::make('token', 'verifyToken')
+        ->whenTexted(function ($message) {
+        })
+        ->process(json_decode(file_get_contents('tests/stubs/webhook_payload_text_message.json'), true));
 })->markTestIncomplete();
+
+it('can determine to process data or not', function () {
+    $query = [
+        'hub' => [
+            'mode' => 'subscribe',
+            'verify_token' => 'meatyhamhock',
+            'challenge' => '1158201444',
+        ],
+    ];
+    expect(Webhook::make('token', 'meatyhamhock')->shouldProcess($query))
+        ->toBeTrue();
+});
