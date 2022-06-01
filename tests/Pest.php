d854e3bcfa2538b1823c67d19a69092bad412391
sawirricardo <sawir.ricardo@gmail.com>
2022-06-01 23:13:28 +0700

    Update Pest.php

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,1 +1,8 @@
 <?php
+
+use Illuminate\Container\Container;
+use Illuminate\Support\Facades\Facade;
+
+$app = new Container();
+$app->singleton('app', Container::class);
+Facade::setFacadeApplication($app);
